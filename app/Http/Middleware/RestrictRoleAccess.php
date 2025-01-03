738d3b6e7c3427f3bcfbeb1e560f38e34a7feefd
fachrianlr <fachrianlr@gmail.com>
2025-01-04 00:41:46 +0700

    add middleware restrictroleaccess

---
+++ app/Http/Middleware/RestrictRoleAccess.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class RestrictRoleAccess
+{
+
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->role->name === 'user') {
+            if ($request->is('admin*')) {
+                abort(403, 'Unauthorized access');
+            }
+        } else if (Auth::check() && Auth::user()->role->name === 'admin') {
+            if ($request->is('user/*')) {
+                abort(403, 'Unauthorized access');
+            }
+        }
+        return $next($request);
+    }
+}
